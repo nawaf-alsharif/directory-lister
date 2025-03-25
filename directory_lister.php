600460ed88045d4a2f16a3e9256cd43f7410cacb
nawaf-alsharif <nawaf@devhubo.com>
2025-03-25 08:01:52 +0300

    Create directory_lister.php

---
+++ directory_lister.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['directory'])) {
+    // الحصول على المدخل من المستخدم
+    $directory = $_POST['directory'];  // تطهير المدخل لتجنب الثغرات الأمنية
+
+    // بناء الأمر باستخدام المدخل
+    $command = "ls -l " . $directory;
+
+    // تنفيذ الأمر باستخدام دالة system()
+     system($command);
+}
+?>
+
+<form method="POST">
+    <label for="directory">أدخل مسار المجلد:</label>
+    <input type="text" id="directory" name="directory" required>
+    <button type="submit">تنفيذ</button>
+</form>
